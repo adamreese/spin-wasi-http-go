5bdeccecf0c3e509f6aa14b282fa2f1734eb5c45
Adam Reese <adam@reese.io>
2025-05-06 11:22:48 -0700

    Add variable sdk to example

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/spinframework/spin-go-sdk/v2/variables"
 	"github.com/spinframework/spin-go-sdk/v2/wit"
 	"github.com/ydnar/wasi-http-go/wasihttp"
 )
@@ -18,6 +19,15 @@ type WasiHTTP struct{}
 
 func (WasiHTTP) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
+
+	// Get variable value `message` defined in spin.toml.
+	val, err := variables.Get("message")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, "message: ", val)
+
 	fmt.Fprintln(w, "Hello World!")
 }
 
